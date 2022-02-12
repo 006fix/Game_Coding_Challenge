6aea264a28d588692522a4ff060db1bf4a62029a
006fix <pye.a.c.a@gmail.com>
2022-02-12 11:44:18 +0000

    hardcoded AI 3 of 11 created.

--- Overall Code/Classes/AI_Classes/Hardcoded_AI/AI_selection.py
+++ Overall Code/Classes/AI_Classes/Hardcoded_AI/AI_selection.py
@@ -2,14 +2,17 @@
 #this file exists to provide a function to instantiate the specific AI classes to the rudimentary AI
 #this makes it easier to modify things as new AI's are added
 
-import Classes.AI_Classes.Hardcoded_AI.Hardcoded_AI_baseclass as base_hardcoded
+#import Classes.AI_Classes.Hardcoded_AI.Hardcoded_AI_baseclass as base_hardcoded
 import Classes.AI_Classes.Hardcoded_AI.Hardcoded_AI_Storage.Full_Random1 as full_random
 import Classes.AI_Classes.Hardcoded_AI.Hardcoded_AI_Storage.Field_Focus_Random2 as field_random
+import Classes.AI_Classes.Hardcoded_AI.Hardcoded_AI_Storage.Field_Focus_Lowest_lvl3 as field_lowest_lvl
 
 def provide_ai(randint, owning_player):
-    if randint >= 50:
+    if randint >= 101:
         #currently we only have one AI, so we'll just use this simple measure
         AI_chosen = full_random.Full_Random_1(owning_player)
-    else:
+    elif randint >= 201:
         AI_chosen = field_random.Field_Focus_2(owning_player)
+    elif randint <= 100:
+        AI_chosen = field_lowest_lvl.Field_Focus_lowest_level_3(owning_player)
     return AI_chosen
---
+++ Overall Code/Classes/AI_Classes/Hardcoded_AI/Hardcoded_AI_Storage/Field_Focus_Lowest_lvl3.py
@@ -0,0 +1,38 @@
+
+#this stores the field focus lowest level random AI
+
+import Classes.AI_Classes.Hardcoded_AI.Hardcoded_AI_baseclass as baseclass
+import random
+
+class Field_Focus_lowest_level_3(baseclass.Underlying_Hardcoded):
+    def __init__(self, owning_player):
+        super().__init__(owning_player)
+
+    def select_building(self, all_possible):
+
+        field_subset = []
+        for item in all_possible:
+            if len(item) == 2:
+                field_subset.append(item)
+
+        #now add in a modifier for field_subset
+        lowest_level = 500
+        #lowest level set to 500, will be overwritten each time
+        for item in field_subset:
+            level = item[1]
+            if level < lowest_level:
+                lowest_level = level
+        #now find all items that meet the criteria of lowest level
+        field_subset2 = []
+        for item in field_subset:
+            level = item[1]
+            if level == lowest_level:
+                field_subset2.append(item)
+
+        if len(field_subset2) > 0:
+            choose_from_list = field_subset2
+        else:
+            choose_from_list = all_possible
+        chosen_action = random.choice(choose_from_list)
+
+        return chosen_action
